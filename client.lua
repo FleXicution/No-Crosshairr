b4a0a1d9f5ae4148792fd5e0358d7c53cf61e35b
FleXicution <46483840+FleXicution@users.noreply.github.com>
2019-01-28 18:44:00 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,13 @@
+local scopedWeapons = 
+{
+    100416529,  -- WEAPON_SNIPERRIFLE
+    205991906,  -- WEAPON_HEAVYSNIPER
+    3342088282  -- WEAPON_MARKSMANRIFLE
+}
+
+Citizen.CreateThread(function()
+    while true do 
+        Wait(0)
+        HideHudComponentThisFrame(14)
+    end
+end)
